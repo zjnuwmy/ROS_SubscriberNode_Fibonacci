2420e0d05b8756833f4aabf0b1dbee7f8fdd2f1e
zjnuwmy <32149719+zjnuwmy@users.noreply.github.com>
2019-06-25 06:53:39 -0700

    Add files via upload

---
+++ mypkg/scripts/test.py
@@ -0,0 +1,18 @@
+class Fibonacci:
+
+    def Fibonacci_Recursion_tool(n):
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return Fibonacci_Recursion_tool(n - 1) + Fibonacci_Recursion_tool(n - 2)
+
+
+    def Fibonacci_Recursion(n):
+        result_list = []
+        for i in range(1, n + 1): result_list.append(Fibonacci_Recursion_tool(i))
+        return result_list
+
+
+
